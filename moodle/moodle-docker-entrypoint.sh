1f0f7be9f8d4f8863cd50684fa650b2ff88c0368
RockeyWereWolf <55295920+RockeyWereWolf@users.noreply.github.com>
2023-04-24 15:17:08 +0400

    Update moodle-docker-entrypoint.sh

--- moodle/moodle-docker-entrypoint.sh
+++ moodle/moodle-docker-entrypoint.sh
@@ -1,19 +1,12 @@
 #!/bin/bash
 
-set -e
+rsync -av /usr/src/moodle/ /var/www/html/
+chown -R www-data:www-data /var/www/html
+chmod -R 0755 /var/www/html
+mkdir -p /var/www/html/moodledata
+chown -R www-data:www-data /var/www/html/moodledata
+exec apache2-foreground
 
-if [ "$1" = 'apache2-foreground' ]; then
-    # Install Moodle if not already installed
-    if [ ! -e /var/www/html/index.php ]; then
-        echo "Moodle not found in $(pwd) - copying now..."
-        rsync -av /usr/src/moodle/ /var/www/html/
-        chown -R www-data:www-data /var/www/html
-        chmod -R 0755 /var/www/html
-        mkdir -p /var/www/html/moodledata
-        chown -R www-data:www-data /var/www/html/moodledata
-    fi
 
-    exec apache2-foreground
-fi
 
-exec "$@"
+
