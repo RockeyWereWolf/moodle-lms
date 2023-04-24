5209c27be3c0c492b818a83a0f46ded6ea9292f0
RockeyWereWolf <55295920+RockeyWereWolf@users.noreply.github.com>
2023-04-24 16:56:59 +0400

    Update moodle-docker-entrypoint.sh

--- moodle/moodle-docker-entrypoint.sh
+++ moodle/moodle-docker-entrypoint.sh
@@ -5,6 +5,8 @@ chown -R www-data:www-data /var/www/html
 chmod -R 0755 /var/www/html
 mkdir -p /var/www/html/moodledata
 chown -R www-data:www-data /var/www/html/moodledata
+chown -R www-data:www-data /var/www
+chmod -R 775 /var/www
 exec apache2-foreground
 
 
